3e0d83629d369cad8deda69f40ee4606564bafe3
Pravin-26 <78830019+Pravin-26@users.noreply.github.com>
2021-03-12 18:28:07 -0500

    Create CYB.php

---
+++ CYB.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+<body>
+    <script src="analytics.js">
+
+    </script>
+
+
+<form action=# method="get">
+<input type="text" name="email"><input type="submit" value="Subscribe!">
+</form>
+
+Thank you for subscription!<br>
+Email <b><script> alert('This is XSS is crafted by Pravin Shinde'); if (window.confirm(document.cookie)){ document.location.href = "http://blackhat.com"; } </script></b> added to mailing list!
+
+</body>
+</html>
+
+</html>
